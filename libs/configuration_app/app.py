9da75c2e24395d7d50ddfd0a9abf0f48e0e81a7d
Stjin <stjin.stijnvandewater@gmail.com>
2022-07-30 15:45:59 +0200

    - Change country

--- libs/configuration_app/app.py
+++ libs/configuration_app/app.py
@@ -91,7 +91,7 @@ def scan_wifi_networks():
 def create_wpa_supplicant(ssid, wifi_key, hidden):
     temp_conf_file = open('wpa_supplicant.conf.tmp', 'w')
 
-    temp_conf_file.write('country=pl\n')
+    temp_conf_file.write('country=nl\n')
     temp_conf_file.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n')
     temp_conf_file.write('update_config=1\n')
     temp_conf_file.write('\n')
